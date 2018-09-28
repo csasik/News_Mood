e1b5a69ba27f86cce101af55924bbc14b1c5f548
csasik <chithra.kumar@gmail.com>
2018-09-27 17:14:22 -0700

    adding first part

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "ND8OfVu3k3QklP0BLMyJDyHPZ"
+consumer_secret = "OMhhAxUnfQ8YQO7wkO1JR7sUWYqZrGqZ7PYvMzPHA8Y6320dYW"
+access_token = "85235272-uIO4LYRxk4eVL4vgcjMsM1AR52Fwwn54U6AivrINy"
+access_token_secret = "v9ene6X26kSR5IruAhWfIaEAZA5WbOqvefipldYoXAWND"
